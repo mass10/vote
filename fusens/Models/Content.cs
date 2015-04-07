746fd69d95ef26bf8202339a65ba0268af68ec27
mass10 <irisawa.masaru@gmail.com>
2015-04-07 23:43:07 +0900

    wip

---
+++ fusens/Models/Content.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace fusens.Models
+{
+	public sealed class Content
+	{
+		[Key]
+		public int ContentId { get; set; }
+		public string GUID { get; set; }
+		public string Text { get; set; }
+		public int Count { get; set; }
+		public DateTime Timestamp { get; set; }
+	}
+}
